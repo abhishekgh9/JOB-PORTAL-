6a8293cc83096c62ace1f71214558191f6665f8a
Abhishek Kumar <abhishekk57078@gmail.com>
2025-07-17 11:58:35 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col ">
+      <Button className="text-white bg-blue-500">Click me</Button>
+    </div>
+  )
+}
+
+export default App
